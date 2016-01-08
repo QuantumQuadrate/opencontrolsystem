660f20eb909e7927fface707dc9dfe63023337cb
iridium5 <pi@raspberrypi.(none)>
2016-01-08 21:21:39 +0000

    echo server tested

--- currentcode/maincontroller/tcp/server.py
+++ currentcode/maincontroller/tcp/server.py
@@ -6,10 +6,12 @@ PORT = 80                 # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
-conn, addr = s.accept()
-print 'Connected by', addr
 while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
-conn.close()
+	conn, addr = s.accept()
+	print 'Connected by', addr
+	while 1:
+		data = conn.recv(1024)
+		print("    recieved data:" + data)
+		if not data: break
+		conn.sendall(data)
+	conn.close()

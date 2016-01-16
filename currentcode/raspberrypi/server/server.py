4958be3e3d74770e027c33f06d70ff5ea5a14204
iridium5 <pi@raspberrypi.(none)>
2016-01-16 23:04:11 +0000

    RPC error handling works

--- currentcode/raspberrypi/server/server.py
+++ currentcode/raspberrypi/server/server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request
 import datetime
 import json
-
+import sys, traceback
 app = Flask(__name__, template_folder='./templates')
 
 
 test_response = {'card1':[]}
+
+
+global_return = ""
 
 @app.route("/")
 def main_serve():
@@ -28,18 +31,32 @@ def hello3():
 		data = request.args.get('request', '')
 		return serve_api(request)
 
-
 def serve_api(params):
 	args = params.args
 	request = args.get('request')
-	if(request == "list cards"):
-		return list_cards()
+	print(request)
+	try:
+		exec(request)
+		return_val = {'result': global_return}
+		return json.dumps(return_val)
+	except Exception, e:
+		trace_back = traceback.format_exc()
+		return_val = {'error': trace_back}
+		return json.dumps(return_val)
 
+def get_remote_code():
+	f = open('server.py', 'r')
+	return f.read()
 def list_cards():
 	cards = []
-	for i in range(0, 10):
+	for i in range(8, 10):
 		cards.append("card"+str(i))
 	r = json.dumps(cards)
+	return r
+
+def test():
+	a = float(1)/0
+	r = json.dumps(a)
 	return r
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port = 80, debug= True)
